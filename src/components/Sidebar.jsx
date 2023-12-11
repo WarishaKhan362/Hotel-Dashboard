8125d927df22db06f74033cc1a702f8177a41188
Warisha Khan <142943552+WarishaKhan362@users.noreply.github.com>
2023-12-11 17:14:46 +0530

    first commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import 
+{ BsGrid1X2Fill, BsFillGrid3X3GapFill, BsPeopleFill, 
+  BsListCheck, BsMenuButtonWideFill, BsFillGearFill}
+ from 'react-icons/bs'
+import { FaHotel } from "react-icons/fa";
+import { FaBed } from "react-icons/fa";
+
+function Sidebar({openSidebarToggle, OpenSidebar}) {
+  return (
+    <aside id="sidebar" className={openSidebarToggle ? "sidebar-responsive": ""}>
+        <div className='sidebar-title'>
+            <div className='sidebar-brand'>
+                <FaHotel className='icon_header'/> Hotel-Dashboard
+            </div>
+            <span className='icon close_icon' onClick={OpenSidebar}>X</span>
+        </div>
+
+        <ul className='sidebar-list'>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsGrid1X2Fill className='icon'/> Dashboard
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <FaBed className='icon'/>Bookings
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsFillGrid3X3GapFill className='icon'/> Cabins
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsPeopleFill className='icon'/> Customers
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsListCheck className='icon'/> Services
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsMenuButtonWideFill className='icon'/> Reports
+                </a>
+            </li>
+            <li className='sidebar-list-item'>
+                <a href="">
+                    <BsFillGearFill className='icon'/> Setting
+                </a>
+            </li>
+        </ul>
+    </aside>
+  )
+}
+
+export default Sidebar
